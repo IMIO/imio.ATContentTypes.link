bf71fa0319dcea684d1fd68e3cf83f2f2babe219
boulch <christophe.boulanger@imio.be>
2015-04-20 10:19:42 +0200

    update some files and clean comments

--- src/imio/ATContentTypes/link/extender.py
+++ src/imio/ATContentTypes/link/extender.py
@@ -1,6 +1,4 @@
-from Products.Archetypes.public import BooleanField, BooleanWidget
-# from Products.Archetypes.public import StringField, StringWidget
-
+from Products.Archetypes.public import BooleanField
 from Products.ATContentTypes.interface import IATLink
 from archetypes.schemaextender.field import ExtensionField
 from archetypes.schemaextender.interfaces import ISchemaExtender
@@ -8,23 +6,13 @@ from zope.component import adapts
 from zope.interface import implements
 
 
-class _ExtensionBooleanField(ExtensionField, BooleanField): pass
-# class _ExtensionStringField(ExtensionField, StringField): pass
+class _ExtensionBooleanField(ExtensionField, BooleanField):
+    pass
 
 
 class LinkTargetExtender(object):
     adapts(IATLink)
     implements(ISchemaExtender)
-
-    #fields = [
-    #    _ExtensionStringField(
-    #        "LinkTargetChoice",
-    #        widget=StringWidget(
-    #            label=u"Open in a new tab (true/false)",
-    #            description=u"Open link in a new tab (true/false)",
-    #        ),
-    #    ),
-    #]
 
     fields = [
         _ExtensionBooleanField(
